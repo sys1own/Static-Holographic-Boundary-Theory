4e38a8969c2c03950d7c591a1e43ff8c7ed9a7d0
sys1own <no.toro2002@gmail.com>
2026-05-06 00:54:12 +0300

    Add sequence_bit_loading function to topological_kernel

--- src/shbt/topological_kernel.py
+++ src/shbt/topological_kernel.py
@@ -21,6 +21,21 @@ from shbt.core.algebra import (
     su3_weight_vector,
 )
 
+
+def sequence_bit_loading(*, lepton_level: int, quark_level: int) -> tuple[tuple[int, int], ...]:
+    lepton_block = ModularKernel(int(lepton_level), "lepton").restricted_block()
+    quark_block = ModularKernel(int(quark_level), "quark").restricted_block()
+    ordering = [
+        (
+            (row_index, column_index),
+            float(abs(lepton_block[row_index, column_index]) ** 2 * abs(quark_block[row_index, column_index]) ** 2),
+        )
+        for row_index in range(lepton_block.shape[0])
+        for column_index in range(lepton_block.shape[1])
+    ]
+    ordering.sort(key=lambda item: (item[1], -item[0][0], -item[0][1]), reverse=True)
+    return tuple(coordinate for coordinate, _ in ordering)
+
 __all__ = [
     "LOW_SU3_WEIGHTS",
     "ModularKernel",
@@ -32,6 +47,7 @@ __all__ = [
     "polar_unitary",
     "predict_delta_cp",
     "rotation_23",
+    "sequence_bit_loading",
     "su2_conformal_weight",
     "su2_modular_s",
     "su2_quantum_dimension",
