b83c354a0479b30233043cdadfa1d675199d6ce5
sys1own <no.toro2002@gmail.com>
2026-05-07 18:37:50 +0300

    Add FrameDependentAlphaAudit and ObserverFrameAudit

--- src/shbt/logic/__init__.py
+++ src/shbt/logic/__init__.py
@@ -2,22 +2,32 @@ from __future__ import annotations
 
 from .observer import (
     AgentLatticeCoordinates,
+    FrameDependentAlphaAudit,
     GeneralRelativityUIAudit,
     HolographicProjectionShift,
     InternalObserver,
+    Observer,
+    ObserverFrameAudit,
     SelfValuationAudit,
     SelfValuationSigma,
+    derive_frame_dependent_alpha,
     derive_general_relativity_ui,
+    derive_observer_frame,
     shift_holographic_projection,
 )
 
 __all__ = [
     "AgentLatticeCoordinates",
+    "FrameDependentAlphaAudit",
     "GeneralRelativityUIAudit",
     "HolographicProjectionShift",
     "InternalObserver",
+    "Observer",
+    "ObserverFrameAudit",
     "SelfValuationAudit",
     "SelfValuationSigma",
+    "derive_frame_dependent_alpha",
     "derive_general_relativity_ui",
+    "derive_observer_frame",
     "shift_holographic_projection",
 ]
