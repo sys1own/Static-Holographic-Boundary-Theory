12d61a2a00685c916c2aea80b9d14a8810483b47
sys1own <no.toro2002@gmail.com>
2026-05-07 23:58:53 +0300

    Add uniqueness proof imports to __init__.py

--- src/shbt/logic/__init__.py
+++ src/shbt/logic/__init__.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
 from .bootstrap import SymmetrySearcher, evaluate_kernel, build_uniqueness_report
+from .uniqueness_proof import (
+    BENCHMARK_PRIME_INDEX,
+    BENCHMARK_REALITY_KERNEL,
+    DistinctionLogicAudit,
+    KernelDivergenceReport,
+    generate_divergence_report,
+)
 from .observer import (
     AgentLatticeCoordinates,
     FrameDependentAlphaAudit,
@@ -28,10 +35,15 @@ __all__ = [
     "SelfValuationAudit",
     "SelfValuationSigma",
     "SymmetrySearcher",
+    "BENCHMARK_PRIME_INDEX",
+    "BENCHMARK_REALITY_KERNEL",
+    "DistinctionLogicAudit",
+    "KernelDivergenceReport",
     "build_uniqueness_report",
     "derive_frame_dependent_alpha",
     "derive_general_relativity_ui",
     "derive_observer_frame",
     "evaluate_kernel",
+    "generate_divergence_report",
     "shift_holographic_projection",
 ]
