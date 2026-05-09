eb65da701f2ca0e4846791f09932cd203e19af5a
sys1own <no.toro2002@gmail.com>
2026-05-09 15:04:48 +0300

    Add resource_paths.py to manage resource paths

---
+++ src/shbt/resource_paths.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from shbt.paths import PACKAGE_ROOT, PAPERS_DIR, ProjectPaths, REPO_ROOT, resolve_resource_path
+
+__all__ = [
+    "PACKAGE_ROOT",
+    "PAPERS_DIR",
+    "ProjectPaths",
+    "REPO_ROOT",
+    "resolve_resource_path",
+]
